e13372d561a80b0ea9d8d75c9d87a41f103f3bec
Hannah <66184980+HannahEG@users.noreply.github.com>
2020-09-02 14:21:13 -0600

    complete

--- app.js
+++ app.js
@@ -13,85 +13,94 @@ function init() {
    })}
    
    init(); 
+  
 
 function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
-    // buildBubbleChart(newSample);
 }
 
-// function buildBubbleChart(sample) {
-//   d3.json("samples.json").then((data) => {
-//     var bubbleChart = {
-//       x: []
-//       y: []
-//       mode: 'markers',
-//       marker: {
-//         size: []
-//       } 
-//     };
-    
-//     var bubbleLayout = {
-//       title: 'Insert',
-//       showlegend: false,
-//       height: 600,
-//       width: 600
-//     };
 
-//     Plotly.newPlot('bubble', [bubbleChart], bubbleLayout)
-//   })
-// };
 
 function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
+    // Build Horizontal Chart
     var sample_data = data.samples; 
     var sampleDataArray = sample_data.filter(sampleObj1 => sampleObj1.id == sample);
-  //console.log(sampleDataArray); 
 
     var sampleBacteriaResults = sampleDataArray[0].sample_values.slice(0,11).reverse();
-  console.log(sampleBacteriaResults);
+    var OTUs = sampleDataArray[0].otu_ids.slice(0,11);
+    var OTUlabels = [sampleDataArray[0].otu_labels.slice(0,11)];
 
-    var OTUs = [sampleDataArray[0].otu_ids.slice(0,11)];
-    var OTUstring = OTUs.forEach(otu => console.log(otu))
-    var OTUlabels = sampleDataArray[0].otu_labels//.slice(0,11);
-console.log(OTUlabels)
     var horizontalBar = [{
               x: sampleBacteriaResults,
-              y: OTUstring,
               type: "bar",
               orientation: 'h',
-              text: [OTUstring],
               hoverinfo: OTUlabels
             }];
     var horizontalLayout = {
         title: "Top 10 Bacterial Species (OTUs)"
+        // yaxis: {
+        //   type: 'category',
+        //   //title: OTUstring
+        //}
       };
-    Plotly.newPlot("bar", horizontalBar, horizontalLayout)
+    Plotly.newPlot("bar", horizontalBar, horizontalLayout);
+
+    
+    // Build Bubble Chart
+    var OTU1s = sampleDataArray[0].otu_ids.slice(0,71).reverse();
+    var sampleBacteriaResults1 = sampleDataArray[0].sample_values.slice(0,71).reverse();
 
     var bubbleChart = {
-      x: OTUs,
-      y: sampleBacteriaResults,
+      x: OTU1s,
+      y: sampleBacteriaResults1,
       mode: 'markers',
       marker: {
-        size: sampleBacteriaResults,
-        color: OTUs
+        size: sampleBacteriaResults1,
+        color: OTU1s
       }
-      //type: 'scatter'
     };
-    
     var bubbleLayout = {
-      title: 'Insert',
+      title: 'Bubble Chart',
       showlegend: false,
+      xaxis: {
+        title: "OTU ID"
+      },
       height: 600,
-      width: 600
+      width: 900
     };
 
-    Plotly.newPlot('bubble', [bubbleChart], bubbleLayout)
+    Plotly.newPlot('bubble', [bubbleChart], bubbleLayout);
+  
 
-    });}
+  // Build gauge for belly button washing
+    var wfreq = data.metadata;
+    var filtered = wfreq.filter(sampleObj2 => sampleObj2.id == sample);
+    var filteredWfreq = wfreq.filter(element => element !=
+     null);
+    var filterArray = filtered[0].wfreq
 
+    console.log(filterArray)  
 
+    var gaugeChart = [{
+      domain: {x: [0,10], y: [0,10]},
+      value: filterArray,
+      title: { text: "Belly Button Wash Frequency"},
+      type: "indicator",
 
+      mode: "gauge+number"
+    }];
+
+    var gaugeLayout = {
+      width: 600,
+      height: 500,
+      margin: {t:10, b:10}
+    };
+
+    Plotly.newPlot('gauge', gaugeChart, gaugeLayout);
+  });
+};
 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
@@ -104,6 +113,6 @@ function buildMetadata(sample) {
     PANEL.html("");
     PANEL.append("h6").text(results);
   });
-}
+};
 
-optionChanged(940)
+optionChanged(940);
